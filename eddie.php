af870bf3bb1a485f65e0dda68894538774f39ff9
Panaite Andrei-Silviu <andrei.panaite@emag.ro>
2025-04-09 11:23:19 +0300

    added dump_v2 with improved backtrace [WIP]

--- eddie.php
+++ eddie.php
@@ -63,6 +63,169 @@ if (!class_exists('Eddie_Logger')) {
             file_put_contents(self::getLogFileFullPath($channel), $contents, FILE_APPEND);
         }
 
+        public function dump_v2(mixed $debug, int|string $channel = null, string $dumpName = null, string $export_type = 'dump'): void
+        {
+            if ($export_type == 'dump' && function_exists('dump')) {
+                ob_start();
+                dump($debug);
+                $dump = ob_get_contents();
+                ob_end_clean();
+            } else if ($export_type == 'var_export') {
+                $dump = var_export($debug, true);
+            } else if ($export_type == 'print_r') {
+                $dump = print_r($debug, true);
+            } else {
+                $dump = print_r($debug, true);
+            }
+            $dumpNameFormatted = !empty($dumpName) ? "<span style='color:#5397da; font-weight: bold;'>$dumpName</span>" : '';
+
+            $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+            $formattedBacktrace = $this->formatDebugBacktrace($backtrace);
+
+            $backtraceClassExploded = explode('\\', $backtrace[1]['class']) ?: [];
+
+            $hrefText = end($backtraceClassExploded) . $backtrace[1]['type'] . $backtrace[1]['function'] . "@" . $backtrace[0]['line'];
+            $remoteFile = $backtrace[0]['file'] ?? '';
+            $remoteLine = (int)($backtrace[0]['line'] ?? 0);
+            $phpStormHref = $this->getPhpStormHref($remoteFile, $remoteLine, $hrefText);
+
+            $project = $this->getProjectName($backtrace[0]['file']);
+
+            $ajax = (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') ? 'AJAX' : "NON-AJAX";
+            $requestUrl = urldecode($_SERVER['REQUEST_URI']);
+
+            $dumpInfo = "<span style='white-space: nowrap'><b>$ajax - $requestUrl</b></span><br>";
+            $dumpDate = date("H:i:s");
+
+            $contents = <<<EOHTML
+
+<style>
+/*pre.sf-dump, pre.sf-dump .sf-dump-default {*/
+/*    background-color: #4d4d4d !important;*/
+/*}*/
+
+/*pre.sf-dump .sf-dump-str {*/
+/*    color: white !important;*/
+/*}*/
+</style>
+
+  <div style="display: flex; flex-wrap: nowrap; height: auto; border-bottom: 2px solid #c2c2c2">
+    <div style="width: 50%; padding: 10px; background-color: #f0f0f0;">
+      <b>{$dumpDate}</b> {$dumpNameFormatted} - {$phpStormHref} - $project
+      {$dump}
+    </div>
+    <div style="width: 50%; padding: 10px; background-color: #e0e0e0; overflow: auto; white-space: nowrap; font-size: 13px">
+       {$dumpInfo}
+       <br>
+       {$formattedBacktrace}
+    </div>
+  </div>
+
+EOHTML;
+
+            $this->addSfDumpAssets(self::getLogFileFullPath($channel));
+            file_put_contents(self::getLogFileFullPath($channel), $contents, FILE_APPEND);
+        }
+
+        public function formatDebugBacktrace(array $backtrace): string
+        {
+            $formattedBacktrace = "";
+            $traceCount = count($backtrace);
+
+            foreach ($backtrace as $index => $item) {
+                if($index === 0) {
+                    $traceCount--;
+                    continue;
+                }
+                if ($item['class']) {
+                    $remoteFile = $backtrace[$index-1]['file'] ?? '';
+                    $remoteLine = (int)($backtrace[$index-1]['line'] ?? 0);
+                    $hrefText = $item['class'] . $item['type'] . $item['function'] . "@" . ($backtrace[$index-1]['line'] ?? '');
+                    $phpStormHref = $this->getPhpStormHref($remoteFile, $remoteLine, $hrefText);
+                    $projectName = $this->getProjectName($remoteFile);
+                } else {
+                    $phpStormHref = $this->getPhpStormHref(
+                        remoteFile: $item['file'],
+                        remoteLine: $backtrace[$index-1]['line'] ?? '',
+                        hrefText: $item['function'] . "@" . ($backtrace[$index-1]['line'] ?? '')
+                    );
+                    $projectName = $this->getProjectName($item['file']);
+                }
+                $projectFormatted = "<span style='width: 57px; background-color: #b5e1c2; display: inline-block; text-align: center; border: 1px solid #a6a6a6;'>$projectName</span>";
+                $traceCountFormatted = "<span style='width: 20px; display: inline-block'>#$traceCount</span>";
+                $formattedBacktrace .= "$projectFormatted $traceCountFormatted $phpStormHref <br>";
+                $traceCount--;
+            }
+
+            return $formattedBacktrace;
+        }
+
+        public function getProjectName(string $remoteFile): string
+        {
+            if (str_contains($remoteFile, 'vendor/eos-lib/sapi-core')) {
+                return 'sapi-core';
+            }
+            elseif (str_contains($remoteFile, 'vendor/eos/lib-eos')) {
+                return 'lib-eos';
+            }
+            elseif (str_contains($remoteFile, 'vendor/eos-lib/sapi-client')) {
+                return 'sapi-client';
+            }
+            elseif (str_contains($remoteFile, '/sapi/') && !str_contains($remoteFile, '/vendor/')) {
+                return 'sapi';
+            }
+            elseif (str_contains($remoteFile, '/front/') && !str_contains($remoteFile, '/vendor/')) {
+                return 'front';
+            }
+            elseif (str_contains($remoteFile, '/www/') && !str_contains($remoteFile, '/vendor/')) {
+                return 'www';
+            }
+            elseif (str_contains($remoteFile, '/vendor/')) {
+                return 'vendor';
+            }
+            return 'N/A';
+        }
+
+
+        private function getPhpStormHref(mixed $remoteFile, int $remoteLine, string $hrefText): string
+        {
+            $phpstormUrl = $this->getPhpstormUrl($remoteFile, $remoteLine);
+            return $phpstormUrl
+                ? "<a href='$phpstormUrl' title='$remoteFile@$remoteLine'>$hrefText</a>"
+                : "<span title='$remoteFile@$remoteLine' style='cursor: hand;'>$hrefText</span>";
+        }
+
+        public function getPhpstormUrl(string $remoteFile, int $line): ?string
+        {
+            $localPath = null;
+            if (str_contains($remoteFile, 'vendor/eos-lib/sapi-core')) {
+                $localPath = '/Users/andrei.panaite/projects/sapi-core' . explode('vendor/eos-lib/sapi-core', $remoteFile)[1];
+            }
+            elseif (str_contains($remoteFile, 'vendor/eos/lib-eos')) {
+                $localPath = '/Users/andrei.panaite/projects/lib-eos' . explode('vendor/eos/lib-eos', $remoteFile)[1];
+            }
+            elseif (str_contains($remoteFile, 'vendor/eos-lib/sapi-client')) {
+                $localPath = '/Users/andrei.panaite/projects/sapi-client' . explode('eos-lib/sapi-client', $remoteFile)[1];
+            }
+            elseif (str_contains($remoteFile, '/sapi/') && !str_contains($remoteFile, '/vendor/')) {
+                if (preg_match("~tags/[^/]+/data/(.*)~", $remoteFile, $matches)) {
+                    $localPath = '/Users/andrei.panaite/projects/sapi/' . $matches[1];
+                }
+            }
+            elseif (str_contains($remoteFile, '/front/') && !str_contains($remoteFile, '/vendor/')) {
+                if (preg_match("~tags/[^/]+/data/(.*)~", $remoteFile, $matches)) {
+                    $localPath = '/Users/andrei.panaite/projects/front/' . $matches[1];
+                }
+            }
+            elseif (str_contains($remoteFile, '/www/') && !str_contains($remoteFile, '/vendor/')) {
+                if (preg_match("~tags/[^/]+/data/(.*)~", $remoteFile, $matches)) {
+                    $localPath = '/Users/andrei.panaite/projects/www/' . $matches[1];
+                }
+            }
+            return $localPath ? "phpstorm://open?file={$localPath}&line={$line}"  : null;
+        }
+
+
         public function clean_backtrace(int|string $channel = null): void
         {
             $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
